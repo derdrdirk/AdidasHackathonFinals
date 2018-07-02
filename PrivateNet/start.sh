f190b9fad9cd01d0954cd119a3ec38b726e880df
Victor Campello <vicmancr@gmail.com>
2018-07-02 17:17:15 +0200

    Removing files from git

--- PrivateNet/start.sh
+++ PrivateNet/start.sh
@@ -1,3 +1,3 @@
 # has to be executed from root folder
 geth init ./PrivateNet/genesis.json --datadir ./PrivateNet/chain
-geth --datadir ./PrivateNet/chain --networkid 9987  --rpc --rpcapi eth,web3,personal, admin --rpccorsdomain "*" --rpcport "8545" --rpcaddress "0.0.0.0"
+geth --datadir ./PrivateNet/chain --networkid 9987  --rpc --rpcapi eth,web3,personal,admin,miner --rpccorsdomain "*" --rpcport "8545" --rpcaddr "0.0.0.0"
