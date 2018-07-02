e451d941fe0cd294a583bfc2f2232bb7d1de06c2
knowledge91 <dirkhornung91@gmail.com>
2018-07-02 13:56:24 +0200

    geth init

--- PrivateNet/start.sh
+++ PrivateNet/start.sh
@@ -1,2 +1,3 @@
 # has to be executed from root folder
-geth --datadir ./PrivateNet/chain --networkid 9987  --rpc --rpcapi eth,web3,personal --rpccorsdomain "*"
+geth init ./PrivateNet/genesis.json --datadir ./PrivateNet/chain
+geth --datadir ./PrivateNet/chain --networkid 9987  --rpc --rpcapi eth,web3,personal --rpccorsdomain "*" --rpcport "8545" --rpcaddress "0.0.0.0"
